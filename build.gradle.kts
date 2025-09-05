5c5d60640eb97dd4fa943cdc110988d057abb14c
yuchenxue <yuchenxue@cute.neko>
2025-09-05 19:59:37 +0800

    chores

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import java.util.Properties
+
 plugins {
     kotlin("jvm")
     id("fml-loom")
+    id("maven-publish")
 }
 
 // Mod Info
@@ -101,6 +104,47 @@ java {
 kotlin {
     compilerOptions {
         jvmToolchain(17)
+    }
+}
+
+// publish to gitlab, you can delete this.
+
+val config = Properties().apply {
+    val file = file("config.properties")
+    if (file.exists()) {
+        file.inputStream().use { load(it) }
+    }
+}
+
+val maven_name: String by project
+val maven_group: String by project
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            groupId = maven_group
+            artifactId = maven_name
+            version = mod_version
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("https://gitlab.com/api/v4/projects/74192719/packages/maven")
+
+            val private_token: String = config.getProperty("private_token") ?: ""
+
+            credentials(HttpHeaderCredentials::class) {
+                name = "Private-Token"
+                value = private_token
+            }
+
+            authentication {
+                create<HttpHeaderAuthentication>("header")
+            }
+        }
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,10 @@ pluginManagement {
             name = "Fabric"
             url = uri("https://maven.fabricmc.net/")
         }
+
+        maven {
+            url = uri("https://gitlab.com/mite-mod/repo/raw/main/maven")
+        }
     }
 
     plugins {
